89c78ec858dbf05c8875e0051e3bd12b99939695
abhishekshivade <abhishekshivade@gmail.com>
2024-05-17 00:31:50 +0530

    comments removed from adminController

--- bankServer/Controllers/adminControllers.js
+++ bankServer/Controllers/adminControllers.js
@@ -7,10 +7,10 @@ import {
 import { dbConnection } from "../Utility/dbConnection.js";
 
 export const createAdmin = (req, res) => {
-  // console.log("creating Account")
+
   const { firstName, lastName, branchName, role, password } = req.body;
 
-    console.log(firstName," ",lastName," ",role," ",password)
+  // console.log(firstName, " ", lastName, " ", role, " ", password);
 
   const encryptedPassword = hashSync(password, 10);
 
@@ -27,58 +27,15 @@ export const createAdmin = (req, res) => {
 
       dbConnection.query(registerAdminQuery, (error, result) => {
         if (error) {
-          console.log(error)
-          res
-            .status(500)
-            .send({
-              message: "Failed to register user, Something went wrong...!",
-            });
+          // console.log(error);
+          res.status(500).send({
+            message: "Failed to register user, Something went wrong...!",
+          });
         } else {
           console.log(result);
           res.status(200).send({ message: "User Registered Successfully...!" });
         }
       });
-
-      // const customerIdQry = `select CustomerID from ${CUST_DETAILS_TABLE} where PanNo='${panNumber}'`;
-
-      // dbConnection.query(customerIdQry, (error, result) => {
-      //   if (error) {
-      //     // console.log(error)
-      //     res.status(500).send({ message: "Failed to Retrive Customer ID" });
-      //   } else {
-      //       // console.log(result);
-      //     const customerId = result[0].CustomerID;
-
-      //   const branchCodeQry = `select branch_code from ${BRANCH_DETAILS_TABLE} where address='${address}'`;
-
-      //   dbConnection.query(branchCodeQry, (error, result) => {
-      //     if (error) {
-      //     //   console.log(error)
-      //       res.status(500).send({ message: "Failed to retrive branch code" });
-      //     } else {
-      //       const branchCode = result[0].branch_code;
-      //       const createAccountQuery = `insert into ${ACC_DETAILS_TABLE} (branch_code,balance,account_type,customerID) values(${branchCode},0.0,'${accountType}',${customerId})`;
-
-      // dbConnection.query(createAccountQuery, (error, result) => {
-      //   if (error) {
-      //     console.log(error)
-      //     res
-      //       .status(500)
-      //       .send({
-      //         message:
-      //           "Failed to create account, Something went wrong...!",
-      //       });
-      //   } else {
-      // console.log(result)
-      //   res
-      //     .status(200)
-      //     .send({ message: "Account Created Successfully...!" });
-      // }
-      // });
-      // }
-      // });
-      // }
-      // });
     }
   });
 };
