1b1466751efc7d1ad28c00018bc184dd7fff53e1
abhishekshivade <abhishekshivade@gmail.com>
2024-05-16 17:34:46 +0530

    customer registration completed

--- bankServer/Routes/customerRoutes.js
+++ bankServer/Routes/customerRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createAccount } from '../Controllers/userControllers'
+import { createAccount } from '../Controllers/customerControllers.js'
 
 export const customerRouter=express.Router()
 
